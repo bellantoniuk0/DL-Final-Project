089bb8c45dbcc811febf2ed1e84467f8181ed9b5
Becca <77752836+swimgirlnv@users.noreply.github.com>
2024-05-05 18:44:57 -0400

    Update fc6.py

--- code/fc6.py
+++ code/fc6.py
@@ -4,23 +4,17 @@ import numpy as np
 class my_fc6(tf.keras.Model):
     def __init__(self, num_classes=300):
         super(my_fc6, self).__init__()
-        self.fc = tf.keras.layers.Dense(num_classes, activation='softmax') #4096
+        self.fc = tf.keras.layers.Dense(num_classes, activation='softmax')
         # either add another dense layer or just a softmax
         #self.relu = tf.keras.layers.ReLU()
 
     def call(self, x):
+        print("Before Dense layer, shape of x:", x.shape)
         x = self.fc(x)
         # x = self.relu(x)
         return x
 
-    def load_weights(self, weights_path):
-        """Load the weights of the Dense layer from a file."""
-        dense_weights = np.load(weights_path)['weights']
-        self.fc.set_weights(dense_weights)
-        # print(f"Dense layer weights loaded from {weights_path}")
 
 def save_fc6_model_weights(model, save_path):
     """Save the weights of the Dense layer to a file."""
-    dense_weights = model.fc.get_weights()
-    np.savez(save_path, weights=dense_weights)
-    # print(f"Dense layer weights saved to {save_path}")
+    model.save_weights(save_path, save_format="tf")
