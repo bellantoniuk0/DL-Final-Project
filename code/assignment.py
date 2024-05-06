f7e01779ff327623f888888ff295859ebdfd12ef
Becca <77752836+swimgirlnv@users.noreply.github.com>
2024-05-05 23:28:02 -0400

    Update assignment.py

--- code/assignment.py
+++ code/assignment.py
@@ -18,20 +18,16 @@ import pandas as pd
 # Constants
 VIDEO_TRAIN_FOLDER = '../data/diving_samples_training' #rn just one video but needs to be all videos 
 VIDEO_TEST_FOLDER = '../data/diving_samples_testing'
-C3D_SAVE_PATH = 'C3D_model'
+C3D_SAVE_PATH = '.C3D_model'
 C3D_ALT_PATH = 'C3D_alt_model'
-DIVE_CLASS_PATH = 'dive_class_model'
 FC6_PATH = 'fc6_model'
 SCORE_REG_PATH = 'score_re_model'
 
 
 def action_classifier(frames, c3d_path):
-    
     model_C3D = C3D()  # Create a new instance of the model
-    # model_C3D.load_weights(c3d_path)  # Load the saved weights
     try:
         model_C3D.load_weights(c3d_path)  # Attempt to load weights
-        print("Weights loaded successfully.")
     except ValueError as e:
         print("Error loading weights:", e)
         print("Review model architecture and checkpoint compatibility.")
@@ -41,24 +37,21 @@ def action_classifier(frames, c3d_path):
     X = np.zeros(shape=(1, 32, 3, 112, 112), dtype=np.float64) #1, 16, 3, 112, 112
     ctr = 0
     
-    # for i in frames2keep:
-    #     temp_slice = frames[i, :, :, :]
-    # #   temp_slice = np.transpose(temp_slice, (1, 2, 0))
-    #     # if temp_slice.shape != (112, 112, 3):
-    #     #     temp_slice = np.transpose(temp_slice, (1, 2, 0))
-    #     if temp_slice.shape != (3, 112, 112):
-    #         print(temp_slice.shape)
-    #         temp_slice = cv2.resize(temp_slice, (112, 112))
-    #         if len(temp_slice.shape) < 3 or temp_slice.shape[2] != 3:  # Check if the frames have three channels
-    #             print("Frame at index", i, "does not have three channels.")
-    #     X[0, ctr, :, :, :] = temp_slice
-    #     ctr += 1
+    for i in frames2keep:
+        temp_slice = frames[i, :, :, :]
+    #   temp_slice = np.transpose(temp_slice, (1, 2, 0))
+        # if temp_slice.shape != (112, 112, 3):
+        #     temp_slice = np.transpose(temp_slice, (1, 2, 0))
+        if temp_slice.shape != (3, 112, 112):
+            temp_slice = cv2.resize(temp_slice, (112, 112))
+            if len(temp_slice.shape) < 3 or temp_slice.shape[2] != 3:  # Check if the frames have three channels
+                X[0, ctr, :, :, :] = temp_slice
+        ctr += 1
 
-    # # Preprocess input
-    # X = X * 255
-    # X = np.flip(X, axis=4)
+    # Preprocess input
+    X = X * 255
+    X = np.flip(X, axis=4)
 
-    print("Shape of input to model:", X.shape)
     # Shape of input to model: (1, 16, 3, 112, 112)
     # Perform prediction
     prediction = model_C3D.predict(X)
@@ -85,8 +78,8 @@ def action_scoring(video, c3d_alt_path, fc6_path, score_reg_path):
     
     # Process video frames
     clip_feats = []
-    print('len of video: ', len(video))
-    print('video shape: ', video.shape)
+    # print('len of video: ', len(video))
+    # print('video shape: ', video.shape)
     for i in np.arange(0, video.shape[2], 16):
         print('i: ', i)
         clip = video[i:i + 16, :, :, :]
@@ -94,8 +87,8 @@ def action_scoring(video, c3d_alt_path, fc6_path, score_reg_path):
         clip_feats.append(clip_feats_temp.numpy())
     
     clip_feats_avg = np.mean(clip_feats, axis=0)
-    print("clip_feats_avg shape:", clip_feats_avg.shape)
 
+    # new_cfa = tf.reduce_mean(clip_feats_avg, axis=[1, 2, 3])
     sample_feats_fc6 = model_my_fc6(clip_feats_avg)
     temp_final_score = model_score_regressor(sample_feats_fc6) * 17
     
@@ -127,8 +120,6 @@ def load_training_data():
     # training_data = np.array(training_data)
     # labels = np.array(labels)
     # save_numpy_data(training_data, labels, "training_data.npz")
-    # print("DATA WRITTEN TO VALIDATION_LABELS.CSV")
-
     return np.array(training_data), np.array(labels)
 
 def save_numpy_data(data, labels, file_path):
@@ -142,13 +133,10 @@ def load_validation_data():
 
     for video_file in os.listdir(VIDEO_TEST_FOLDER):
         video_file_path = os.path.join(VIDEO_TEST_FOLDER, video_file)
-        #print(f"Processing video file: {video_file_path}")
 
         with open(video_file_path, 'rb') as file:
-            frames = preprocess_video(file, input_resize=(171, 128), H=112) # 160, 3, 112, 112
-            print('frames shape: ', frames.shape)
-            print(frames.dtype)
-            raise ValueError('stop')
+            frames = preprocess_video(file, input_resize=(171, 128), H=112) 
+            # 160, 3, 112, 112
             validation_data.append(frames)
 
             if video_file not in label_to_int:
@@ -160,7 +148,6 @@ def load_validation_data():
     # validation_data = np.array(validation_data)
     # labels = np.array(labels)
     # save_numpy_data(validation_data, labels, "validation_data.npz")
-    # print("DATA WRITTEN TO VALIDATION_LABELS.NPZ")
     return np.array(validation_data), np.array(labels)
 
 loss_function = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
@@ -240,30 +227,30 @@ def main():
 
     c3d_model = C3D()    
     # Train the model
-    train_model(c3d_model, train_dataset, val_dataset, epochs=0, model_name='c3d')
+    train_model(c3d_model, train_dataset, val_dataset, epochs=10, model_name='c3d')
     save_c3d_model_weights(c3d_model, C3D_SAVE_PATH)
     
     c3d_alt_model = C3D_altered()
     #TRAIN HERE
     # print('TRAINING C3D ALT MODEL')
-    # train_model(c3d_alt_model, train_dataset, val_dataset, epochs=0, model_name='c3d_alt')
-    # save_c3d_alt_model_weights(c3d_alt_model, C3D_ALT_PATH)
+    train_model(c3d_alt_model, train_dataset, val_dataset, epochs=10, model_name='c3d_alt')
+    save_c3d_alt_model_weights(c3d_alt_model, C3D_ALT_PATH)
     
    
     fc6_model = my_fc6()
     #TRAIN HERE
     # print('TRAINING FC6')
     # loss_function_fc6 = tf.keras.losses.SparseCategoricalCrossentropy() # from_logits=True
-    # train_model(fc6_model, train_dataset, val_dataset, epochs=10, model_name='fc6')
-    # save_fc6_model_weights(fc6_model, FC6_PATH)
+    train_model(fc6_model, train_dataset, val_dataset, epochs=10, model_name='fc6')
+    save_fc6_model_weights(fc6_model, FC6_PATH)
     
 
     # This does work! Loss is horrifically high but it works
     score_re_model = ScoreRegressor()
     #TRAIN HERE 
     # print('TRAINING SCORE REGRESSOR')
-    # train_model(score_re_model, train_dataset, val_dataset, epochs=10, model_name='score_regressor')
-    # save_score_reg_model_weights(score_re_model, SCORE_REG_PATH)
+    train_model(score_re_model, train_dataset, val_dataset, epochs=10, model_name='score_regressor')
+    save_score_reg_model_weights(score_re_model, SCORE_REG_PATH)
 
 
     # Now use the saved model path for classification
@@ -275,7 +262,7 @@ def main():
     all_scores = []
 
     # Loop through all videos in the validation dataset
-    for index, frames in enumerate(train_array):
+    for index, frames in enumerate(valid_array):
         classified_action = action_classifier(frames, C3D_SAVE_PATH)
         all_classifications.append(classified_action)
         print(f"Classified Action for video {index + 1}: ", classified_action)
@@ -295,6 +282,8 @@ def save_results(classifications, scores):
     print('Results saved to video_analysis_results.csv')
 
     #SUM AND PRINT FINAL ACTION SCORE
+    # sum from model is total loss from action classifier and action scorer
+    # action scorer gives top five
 
 if __name__ == '__main__':
     main()
